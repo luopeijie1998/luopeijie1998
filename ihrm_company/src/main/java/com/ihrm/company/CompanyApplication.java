66b7448f1415b984c9bbdefaab0c261a01629489
lpeijie <784127363@qq.com>
2021-09-21 10:47:51 +0800

    eureka注册中心搭建，及微服务注册

--- ihrm_company/src/main/java/com/ihrm/company/CompanyApplication.java
+++ ihrm_company/src/main/java/com/ihrm/company/CompanyApplication.java
@@ -1,20 +1,16 @@
 package com.ihrm.company;
 
-
-
-import com.ihrm.common.utils.IdWorker;
-import jdk.internal.org.objectweb.asm.tree.analysis.Value;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.context.annotation.Bean;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 
 //1.springboot的包扫描
 @SpringBootApplication(scanBasePackages = "com.ihrm")
 //2.配置jpa注解的扫描
 @EntityScan(value = "com.ihrm.domain.company")
-
+@EnableEurekaClient
 public class CompanyApplication {
 
     public static void main(String[] args) {
--- ihrm_system/src/main/java/com/ihrm/system/SystemApplication.java
+++ ihrm_system/src/main/java/com/ihrm/system/SystemApplication.java
@@ -5,6 +5,7 @@ import com.ihrm.common.utils.JwtUtil;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 
 import javax.persistence.Entity;
@@ -15,6 +16,7 @@ import javax.persistence.Entity;
 @SpringBootApplication(scanBasePackages = "com.ihrm")
 //2.配置jap注解的扫描
 @EntityScan(value = "com.ihrm.domain.system")
+@EnableEurekaClient
 public class SystemApplication {
     public static void main(String[] args) {
         SpringApplication.run(SystemApplication.class,args);
